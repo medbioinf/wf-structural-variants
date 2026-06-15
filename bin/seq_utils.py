60fa08ccb90a138b56da6a4c8580339742563271
Jonah Kapski <jonah.kapski@edu.ruhr-uni-bochum.de>
2026-06-15 05:56:25 +0000

    update dotplot stride size calculation function

--- bin/seq_utils.py
+++ bin/seq_utils.py
@@ -8,9 +8,37 @@ Wang, S., Xu, T., Zhang, P. & Ye, K. Population-level structural variant
 characterization using pangenome graphs. Nat Genet (2026). 
 https://doi.org/10.1038/s41588-026-02538-6
 
-Modified, refactored and optimized for Nextflow integration.
+Modified and refactored for Nextflow integration.
 Copyright (c) 2026 Jonah Kapski <Jonah.Kapski@edu.ruhr-uni-bochum.de>
 """
+
+def reverse_complement_seq(seq):
+    """
+    Returns the reverse complement of a DNA sequence.
+    """
+    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A', 'N': 'N',
+                  'a': 't', 'c': 'g', 'g': 'c', 't': 'a', 'n': 'n'}
+    return ''.join(complement.get(base, base) for base in reversed(seq))
+
+
+def calculate_stride_size(seq_x, seq_y):
+    """
+    Calculates the stride size for dotplot alignment to keep the final matrix dimension manageable.
+    """
+    max_seq_len = max([len(seq_x), len(seq_y)])
+
+    if max_seq_len <= 1500:
+        stride_size = 1
+    elif max_seq_len <= 15000:
+        stride_size = 10
+    elif max_seq_len <= 75000:
+        stride_size = 30
+    elif max_seq_len <= 100000:
+        stride_size = 50
+    else:
+        stride_size = 100
+
+    return stride_size
 
 
 def is_kmer_similar(kmer1, kmer2, mismatch_thresh=0):
@@ -21,31 +49,3 @@ def is_kmer_similar(kmer1, kmer2, mismatch_thresh=0):
         return False
     mismatch_cnt = sum(b1 != b2 for b1, b2 in zip(kmer1, kmer2))
     return mismatch_cnt <= mismatch_thresh
-
-
-def calculate_stride_size(seq_len_x, seq_len_y, max_matrix_dim=1500):
-    """
-    Calculates the optimal stride size to ensure that the resulting 2D matrix
-    does not exceed a defined maximum pixel dimension (default: 1500x1500).
-    
-    (Replaces Swave's hard thresholds with a smooth calculation to keep matrix dimensions within the desired target range.)
-    """
-    max_seq_len = max(seq_len_x, seq_len_y)
-    
-    # If the max seq length fits within the max matrix dimension, we can use a stride of 1 (no downsampling)
-    if max_seq_len <= max_matrix_dim:
-        return 1
-    
-    # Otherwise: divide the sequence length by the desired matrix dimension and round up (e.g., 150.000 bp / 1500 max dim = stride of 100)
-    stride_size = int(max_seq_len / max_matrix_dim)
-    
-    return max(1, stride_size)
-
-
-def reverse_complement_seq(seq):
-    """
-    Returns the reverse complement of a DNA sequence.
-    """
-    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A', 'N': 'N',
-                  'a': 't', 'c': 'g', 'g': 'c', 't': 'a', 'n': 'n'}
-    return ''.join(complement.get(base, base) for base in reversed(seq))
