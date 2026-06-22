8b9184d1352feb4b759ce873e8a5213b62338df5
Jonah Kapski <jonah.kapski@edu.ruhr-uni-bochum.de>
2026-06-22 13:37:41 +0000

    add main executable generate dotplots script

---
+++ swave/src/generate_dotplots.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""
+Based on the Swave software package (https://github.com/skandavb/Swave).
+Originally licensed under the GPL-3.0.
+
+Publication:
+Wang, S., Xu, T., Zhang, P. & Ye, K. Population-level structural variant 
+characterization using pangenome graphs. Nat Genet (2026). 
+https://doi.org/10.1038/s41588-026-02538-6
+
+Modified and refactored for Nextflow integration.
+Copyright (c) 2026 Jonah Kapski <Jonah.Kapski@edu.ruhr-uni-bochum.de>
+"""
+
+import sys
+import argparse
+import logging
+
+from src.generate_dotplots_mod.dotplot_processing import process_sample_alleles_to_matrices
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(levelname)s] %(message)s',
+    stream=sys.stdout
+)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generates 2D-dotplot matrices (REF2REF, REF2ALT, ALT2ALT)"
+                                     "from pangenome graph alleles and exports them into a compressed .npz archive.")
+    
+    parser.add_argument('--alt_fasta', required=True, help="Path to the extracted alleles fasta (ALT).")
+    parser.add_argument('--ref_fasta', required=True, help="Path to the fasta file of the original reference genome (REF).")
+    parser.add_argument('--gfa_fasta', required=True, help="Path to the pangenome gfa fasta file.")
+    parser.add_argument('--npz_out_prefix', required=True, help="Output prefix for the generated .npz file and dotplot images.")
+    parser.add_argument('--img_out_prefix', required=True, help="Output prefix for the generated dotplot images.")
+    
+    # Optional parameters
+    parser.add_argument('--kmer_size', type=int, default=30, help="K-mer size (default: 30).")
+    parser.add_argument('--max_sv_size', type=int, default=100000, help="Maximum SV size to process (default: 100000).")
+    parser.add_argument('--spec_path', help="Process only a specific path or snarl ID.")
+    parser.add_argument('--save_dotplot_images', action='store_true', help="Saves PNG dotplot images for each snarl.")
+    parser.add_argument('--skip_forward', action='store_true', help="Skip forward k-mer matches in dotplot matrix generation.")
+    parser.add_argument('--skip_reverse', action='store_true', help="Skip reverse k-mer matches in dotplot matrix generation.")
+    
+    options = parser.parse_args()
+    
+    logging.info(f"Starting dotplot matrix generation for {options.alt_fasta}...")
+    
+    process_sample_alleles_to_matrices(
+        alt_fasta_path=options.alt_fasta,
+        ref_fasta_path=options.ref_fasta,
+        pangenome_gfa_fasta_path=options.gfa_fasta,
+        options=options
+    )
+    
+    logging.info(f"Dotplot matrix generation for {options.alt_fasta} completed.")
+    
+    sys.exit(0)
