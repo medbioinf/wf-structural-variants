562d5b696359b2031df5ca461a70834fd5801748
Jonah Kapski <jonah.kapski@edu.ruhr-uni-bochum.de>
2026-06-23 14:17:37 +0000

    add base file structure for swave predict

---
+++ swave/src/predict.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Based on the Swave software package (https://github.com/skandavb/Swave).
+Originally licensed under the GPL-3.0.
+
+Publication:
+Wang, S., Xu, T., Zhang, P. & Ye, K. Population-level structural variant 
+characterization using pangenome graphs. Nat Genet (2026). 
+https://doi.org/10.1038/s41588-026-02538-6
+
+Modified and refactored for Nextflow integration.
+Copyright (c) 2026 Jonah Kapski <Jonah.Kapski@edu.ruhr-uni-bochum.de>
+"""
+
+import sys
+import logging
+import argparse
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(levelname)s] %(message)s',
+    stream=sys.stdout
+)
+
+
+if __name__ == "__main__":
+    pass
